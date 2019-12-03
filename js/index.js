cc3e6abefe497f6367de5267079c004c10f12944
Andrea Harris <Andrea@zacs-imac.lan>
2019-12-03 15:59:12 -0600

    2 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+const busImg = document.querySelector(".intro img")
+
+busImg.addEventListener("mouseenter", () => {
+    busImg.style.transform ="scale(0.5)";
+    busImg.style.transition ="transform 2s"
+})
+
+busImg.addEventListener("mouseleave",() =>{
+    busImg.style.transform ="scale(1)"
+})
+
+
+const Logo= document.querySelector(".logo-heading")
+
+Logo.addEventListener("click", () => {
+    Logo.style.fontSize ="7rem"; 
+    Logo.style.color="#ffd800";
+})
