e0e5907766b3cdb81a0af58a9c1b6a86051c64be
Andrea Harris <Andrea@zacs-imac.lan>
2019-12-03 19:07:20 -0600

    9

--- js/index.js
+++ js/index.js
@@ -43,7 +43,34 @@ swapImg.addEventListener("click", () => {
 const DoubleImg = document.querySelector("h2")
 
 DoubleImg.addEventListener("dblclick", () =>{
-    DoubleImg.style.transform ="rotate(20deg)";
+    DoubleImg.style.transform ="rotate(30deg)";
 })
 
+//7//
+const Button= document.querySelectorAll(".btn").forEach (e =>{
+    e.addEventListener("mousedown", function(){
+    e.style.transform="scale(1.5)";
+    e.style.transition ="transform 3s"
 
+    })
+})
+
+//8//
+
+const destinationImg = document.querySelectorAll(".content-destination img")
+
+destinationImg.addEventListener("focus", () =>{
+    destinationImg.style.backgroundColor= "#bd5734";
+})
+
+//9//
+const topimage= document.querySelectorAll('.img-content')[0];
+
+topimage.addEventListener("load", () => {
+    topimage.style.backgroundColor = "pink"; 
+})
+
+//10//
+document.querySelector('body').addEventListener('keydown', (e)=>{
+    alert(`You pressed ${e.key}`)
+})
